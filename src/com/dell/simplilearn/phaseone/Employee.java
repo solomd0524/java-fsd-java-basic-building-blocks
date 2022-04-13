1904a0e84ccbd90ccbfe555ffeb08e9c8f53706c
Solomon, David <david.solomon@dell.com>
2022-04-13 07:00:33 -0400

    adding employee

---
+++ src/com/dell/simplilearn/phaseone/Employee.java
@@ -0,0 +1,38 @@
+package com.dell.simplilearn.phaseone;
+
+public class Employee {
+
+    private String name;
+    private Long id;
+    private Long salary;
+
+    public void employee(String name, Long id, Long salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+}
